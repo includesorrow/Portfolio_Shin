9184d346c98d816630772bc8f7fa180ada3e6b68
includesorrow <includesorrow@gmail.com>
2019-06-27 15:46:02 +0900

    2019.06.27

--- Data/SpringProject/MidProject22/MidProject/src/kr/or/kosta/mvc/dao/DynamicExampleDaoInter.java
+++
@@ -1,9 +0,0 @@
-package kr.or.kosta.mvc.dao;
-
-import kr.or.kosta.dto.MovieVO;
-
-public interface DynamicExampleDaoInter {
-	public MovieVO getMovieList(MovieVO vo);
-	public MovieVO getMovieTitleList(MovieVO vo);
-	public MovieVO getMovieList3(MovieVO vo);
-}
